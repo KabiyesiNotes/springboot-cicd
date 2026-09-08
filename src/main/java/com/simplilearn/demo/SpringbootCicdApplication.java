9c7b95e5e6102148a50bfc05db88a1260b578b19
KabiyesiNotes <kevin.okeleke@gmail.com>
2026-09-09 00:18:25 +0100

    Create SpringbootCicdApplication.java

---
+++ src/main/java/com/simplilearn/demo/SpringbootCicdApplication.java
@@ -0,0 +1,25 @@
+package com.simplilearn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCicdApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Inventory Management System is running successfully!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
